a4acb056f3fb7e243b54d90cc2e11c9e6d1876d5
Sravanireddy@123 <sravanireddy1066>
2022-03-29 17:43:44 +0530

    updated registry

--- MicroServicesRegistry/src/main/java/com/example/demo/MicroServicesRegistryApplication.java
+++ MicroServicesRegistry/src/main/java/com/example/demo/MicroServicesRegistryApplication.java
@@ -10,6 +10,7 @@ public class MicroServicesRegistryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroServicesRegistryApplication.class, args);
-	}
+		System.out.println("");	
+		}
 
 }
